077838eff1d536ec7aa6ace3d83e4bfa0eb12705
Jesus Moreno <jesusrmor94@gmail.com>
2016-04-14 03:09:00 -0400

    :sparkles: Add color palatte

---
+++ colors.go
@@ -0,0 +1,177 @@
+package main
+
+var allColors = [][]string{
+	reds,
+	purples,
+	deepPurples,
+	green,
+	blue,
+	blueGrey,
+	cyan,
+	lightBlue,
+	lightGreen,
+	pinks,
+}
+
+var reds = []string{
+	"#D32F2F",
+	"#C62828",
+	"#B71C1C",
+	"#D50000",
+}
+
+var pinks = []string{
+	"#D81B60",
+	"#C2185B",
+	"#AD1457",
+	"#880E4F",
+	"#C51162",
+}
+
+var purples = []string{
+	"#9C27B0",
+	"#8E24AA",
+	"#7B1FA2",
+	"#6A1B9A",
+	"#4A148C",
+}
+
+var deepPurples = []string{
+	"#D1C4E9",
+	"#B39DDB",
+	"#9575CD",
+	"#7E57C2",
+	"#673AB7",
+	"#5E35B1",
+	"#512DA8",
+	"#4527A0",
+	"#311B92",
+	"#B388FF",
+	"#7C4DFF",
+	"#651FFF",
+	"#6200EA",
+}
+
+var indigo = []string{
+	"#C5CAE9",
+	"#9FA8DA",
+	"#7986CB",
+	"#5C6BC0",
+	"#3F51B5",
+	"#3949AB",
+	"#303F9F",
+	"#283593",
+	"#1A237E",
+	"#8C9EFF",
+	"#536DFE",
+	"#3D5AFE",
+	"#304FFE",
+}
+
+var blue = []string{
+	"#BBDEFB",
+	"#90CAF9",
+	"#64B5F6",
+	"#42A5F5",
+	"#2196F3",
+	"#1E88E5",
+	"#1976D2",
+	"#1565C0",
+	"#0D47A1",
+	"#82B1FF",
+	"#448AFF",
+	"#2979FF",
+	"#2962FF",
+}
+
+var lightBlue = []string{
+	"#B3E5FC",
+	"#81D4fA",
+	"#4fC3F7",
+	"#29B6FC",
+	"#03A9F4",
+	"#039BE5",
+	"#0288D1",
+	"#0277BD",
+	"#01579B",
+	"#80D8FF",
+	"#40C4FF",
+	"#00B0FF",
+	"#0091EA",
+}
+
+var cyan = []string{
+	"#B2EBF2",
+	"#80DEEA",
+	"#4DD0E1",
+	"#26C6DA",
+	"#00BCD4",
+	"#00ACC1",
+	"#0097A7",
+	"#00838F",
+	"#006064",
+	"#84FFFF",
+	"#18FFFF",
+	"#00E5FF",
+	"#00B8D4",
+}
+
+var teal = []string{
+	"#B2DFDB",
+	"#80CBC4",
+	"#4DB6AC",
+	"#26A69A",
+	"#009688",
+	"#00897B",
+	"#00796B",
+	"#00695C",
+	"#004D40",
+	"#A7FFEB",
+	"#64FFDA",
+	"#1DE9B6",
+	"#00BFA5",
+}
+
+var green = []string{
+	"#C8E6C9",
+	"#A5D6A7",
+	"#81C784",
+	"#66BB6A",
+	"#4CAF50",
+	"#43A047",
+	"#388E3C",
+	"#2E7D32",
+	"#1B5E20",
+	"#B9F6CA",
+	"#69F0AE",
+	"#00E676",
+	"#00C853",
+}
+
+var lightGreen = []string{
+	"#DCEDC8",
+	"#C5E1A5",
+	"#AED581",
+	"#9CCC65",
+	"#8BC34A",
+	"#7CB342",
+	"#689F38",
+	"#558B2F",
+	"#33691E",
+	"#CCFF90",
+	"#B2FF59",
+	"#76FF03",
+	"#64DD17",
+}
+
+var blueGrey = []string{
+	"#CFD8DC",
+	"#B0BBC5",
+	"#90A4AE",
+	"#78909C",
+	"#607D8B",
+	"#546E7A",
+	"#455A64",
+	"#37474F",
+	"#263238",
+}
--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"image"
 	"log"
+	"math/rand"
 	"os"
 	"strconv"
 
@@ -23,13 +24,23 @@ func isEven(n int) bool {
 	return n%2 == 0
 }
 
-func draw(m *gg.Context, pixels [7][7]bool, fg colorful.Color) {
+func random(min, max int) int {
+	return rand.Intn(max-min) + min
+}
+
+func draw(m *gg.Context, pixels [7][7]bool) {
 	offset := m.Width() / 7
 	var x float64
 	var y float64
 	var w float64
 	var h float64
-	bg, err := colorful.Hex("#F0F0F0")
+	colorList := allColors[random(0, len(allColors))]
+	bg, err := colorful.Hex("#fefefe")
+	cHex := colorList[random(0, len(colorList))]
+	c, err := colorful.Hex(cHex)
+	if err != nil {
+		log.Fatal(err)
+	}
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -40,7 +51,7 @@ func draw(m *gg.Context, pixels [7][7]bool, fg colorful.Color) {
 			w = float64((i + 1) * offset)
 			h = float64((j + 1) * offset)
 			if pixels[i][j] {
-				m.SetColor(fg)
+				m.SetColor(c)
 				m.DrawRectangle(x, y, w, h)
 				m.Fill()
 			} else {
@@ -68,7 +79,7 @@ func getPixels(hash string, flag bool) [7][7]bool {
 	for i := 1; i < 6; i++ {
 		for j := 1; j < 6; j++ {
 			char := hash[i*5+j]
-			num, _ := strconv.ParseInt("0x"+string(char), 0, 10)
+			num, _ := strconv.ParseInt("0x"+string(char), 0, 8)
 			if flag {
 				pixels[i][j] = isEven(int(num))
 			} else {
@@ -85,19 +96,15 @@ func generate(c *cli.Context) {
 		log.Fatal("fatal: no string provided")
 	}
 	hash := generateHash(txt)
-	colorCode := hash[:6]
-	num, _ := strconv.ParseInt("0x"+string(hash[0]), 0, 10)
-	flag := isEven(int(num))
+	seed, _ := strconv.ParseInt("0x"+string(hash[0:8]), 0, 64)
+	rand.Seed(seed)
+	flag := isEven(int(seed))
 	width, height := 420, 420
 	m := image.NewRGBA(image.Rect(0, 0, width, height))
-	fg, err := colorful.Hex("#" + colorCode)
-	if err != nil {
-		log.Fatal(err)
-	}
 	ctx := gg.NewContextForImage(m)
 	basePixels := getPixels(hash, flag)
 	finalPixels := reflectPixels(basePixels)
-	draw(ctx, finalPixels, fg)
+	draw(ctx, finalPixels)
 	ctx.SavePNG("identicon.png")
 }
 
